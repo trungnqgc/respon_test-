d68019ef7bd0bdfed56c39a9988bd1960ee6093d
trungnqgc <trungnq2671996@gmail.com>
2017-01-18 01:08:38 +0700

    Update JavaApplication7.java

--- JavaApplication7/src/javaapplication7/JavaApplication7.java
+++ JavaApplication7/src/javaapplication7/JavaApplication7.java
@@ -16,6 +16,8 @@ public class JavaApplication7 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int a=5;
+        int b=6;
     }
     
 }
